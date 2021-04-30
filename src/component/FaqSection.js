52048624814d9d080220b9a5ecdf4a24caf1396b
gavin-rilee <you@example.com>
2021-04-30 16:04:07 -0400

    Faq

---
+++ src/component/FaqSection.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const FaqSection = () => {
+    return (
+        <div>
+            <div className="faq">
+                <h2>Any Questions <span>FAQ</span></h2>
+                <div className="question">
+                    <h4>How Did I Start</h4>
+                    <div className="answer">
+                        <p>When COVID start I researched Lambda School to find something fitting for me </p>
+                        <p>Fran and I need a fresh start</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Why did you get into tech?</h4>
+                    <div className="answer">
+                        <p>I honestly did not have much intrest but everything in the workld is moving that way and now i lovce it </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>How old are you</h4>
+                    <div className="answer">
+                        <p>485632</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Can I have Snurch?</h4>
+                    <div className="answer">
+                        <p>NO</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,12 +3,14 @@ import React from 'react'
 //Page Components
 import AboutSection from '../component/AboutSection';
 import ServicesSection from '../component/ServicesSection';
+import FaqSection from '../component/FaqSection';
 
 const AboutMe = () => {
     return (
         <>
           <AboutSection/>  
           <ServicesSection/>
+          <FaqSection/>
         </> 
     )
 }
